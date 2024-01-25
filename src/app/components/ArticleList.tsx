aedb469dfcf93d37b05cab75481caab9c6fb52b2
konapeso <n2kgt1@gmail.com>
2024-01-25 12:38:20 +0900

    change css style

--- src/app/components/ArticleList.tsx
+++ src/app/components/ArticleList.tsx
@@ -5,8 +5,8 @@ import Image from "next/image";
 const ArticleList = () => {
   return (
     <div>
-      <article>
-        <Link href="#">
+      <article className="shadow my-4">
+        <Link href="#" className="hover:opacity-75">
           <Image
             src="https://source.unsplash.com/collection/1346951/1000*500?sig=1"
             alt=""
@@ -14,17 +14,57 @@ const ArticleList = () => {
             height={300}
           />
         </Link>
-        <div>
-          <Link href="#">Technology</Link>
-          <Link href="#">Next.jsの勉強中</Link>
-          <p>By konamiota, Published on 2024/01/25</p>
-          <Link href="#">
+        <div className="bg-white flex flex-col justify-start p-6">
+          <Link href="#" className="text-blue-700 pb-4 font-bold">
+            Technology
+          </Link>
+          <Link
+            href="#"
+            className="text-slate-900 text-3xl font-bold hover:text-gray-700"
+          >
+            Next.jsの勉強中
+          </Link>
+          <p className="text-sm pb-3 text-slate-900">Published on 2024/01/25</p>
+          <Link href="#" className="text-slate-900 pb-6">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
             varius enim in eros elementum tristique. Duis cursus, mi quis
             viverra ornare, eros dolor interdum nulla, ut commodo diam libero
             vitae erat.
           </Link>
-          <Link href="#">続きを読む</Link>
+          <Link href="#" className="text-pink-800 hover:text-black">
+            続きを読む
+          </Link>
+        </div>
+      </article>
+      <article className="shadow my-4">
+        <Link href="#" className="hover:opacity-75">
+          <Image
+            src="https://source.unsplash.com/collection/1346951/1000*500?sig=2"
+            alt=""
+            width={1280}
+            height={300}
+          />
+        </Link>
+        <div className="bg-white flex flex-col justify-start p-6">
+          <Link href="#" className="text-blue-700 pb-4 font-bold">
+            Technology
+          </Link>
+          <Link
+            href="#"
+            className="text-slate-900 text-3xl font-bold hover:text-gray-700"
+          >
+            Next.jsの勉強中
+          </Link>
+          <p className="text-sm pb-3 text-slate-900">Published on 2024/01/25</p>
+          <Link href="#" className="text-slate-900 pb-6">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            varius enim in eros elementum tristique. Duis cursus, mi quis
+            viverra ornare, eros dolor interdum nulla, ut commodo diam libero
+            vitae erat.
+          </Link>
+          <Link href="#" className="text-pink-800 hover:text-black">
+            続きを読む
+          </Link>
         </div>
       </article>
     </div>
