7b34e2934b6c5822bc99d414effe319e525ecff0
konapeso <n2kgt1@gmail.com>
2024-01-25 16:24:50 +0900

    change to correct data

--- src/app/articles/[id]/page.tsx
+++ src/app/articles/[id]/page.tsx
@@ -4,18 +4,20 @@ import { getDetailArticle } from "@/blogAPI";
 
 const Article = async ({ params }: { params: { id: string } }) => {
   const detailArticle = await getDetailArticle(params.id);
-  console.log(detailArticle);
+
   return (
     <div className="max-w-3xl mx-auto p-5">
       <Image
-        src="https://source.unsplash.com/collection/1346951/1000*500?sig=1"
+        src={`https://source.unsplash.com/collection/1346951/1000x500?sig=${detailArticle.id}`}
         alt=""
         width={1280}
         height={300}
       />
-      <h1 className="text-4xl text-center mb-10 mt-10">ここがタイトルです。</h1>
+      <h1 className="text-4xl text-center mb-10 mt-10">
+        {detailArticle.title}
+      </h1>
       <div className="text-lg leading-relaxed">
-        <p>ここが本文です。</p>
+        <p>{detailArticle.content}</p>
       </div>
     </div>
   );
