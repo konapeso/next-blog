bfd55026508764703e3e737f666ece3c70bac65f
konapeso <n2kgt1@gmail.com>
2024-01-25 16:16:02 +0900

    create detail API

--- src/blogAPI.ts
+++ src/blogAPI.ts
@@ -1,4 +1,5 @@
 import { Article } from "./types";
+import { notFound } from "next/navigation";
 
 export const getAllArticles = async (): Promise<Article[]> => {
   const res = await fetch("http://localhost:3001/posts", { cache: "no-store" }); //SSR
@@ -11,4 +12,23 @@ export const getAllArticles = async (): Promise<Article[]> => {
 
   const articles = await res.json();
   return articles;
+};
+
+export const getDetailArticle = async (id: string): Promise<Article> => {
+  const res = await fetch(`http://localhost:3001/posts/${id}`, {
+    next: { revalidate: 60 },
+  }); //SSR -> ISR
+
+  if (res.status === 404) {
+    notFound();
+  }
+
+  if (!res.ok) {
+    throw new Error("Errorが発生しました");
+  }
+
+  await new Promise((resolve) => setTimeout(resolve, 1000)); //SSR
+
+  const article = await res.json();
+  return article;
 };
